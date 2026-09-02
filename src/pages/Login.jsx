3643ebc31092f6c1248ea0a7c010e96bbed05fe9
farizirvansyah <fariz.irvansyah@gmail.com>
2026-09-02 14:42:31 +0700

    feat: implement login functionality, enhance user management with modal, and update user table structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
 import { Form, Button, Card, Container, Row, Col, Alert } from "react-bootstrap";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+
+    const navigate = useNavigate();
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const response = await fetch(`http://localhost:8000/api/login`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+                body: JSON.stringify({ email, password })
+            });
+            const data = await response.json();
+            if (response.ok) {
+                localStorage.setItem('token', data.token);
+                Navigate('/dashboard');
+
+            }
+        } catch (error) {
+            console.log(error);
+            setError('Server Error')
+            
+        }
+    }
+
     return (
         <Container className="d-flex justify-content-center align-items-center">
             <Row className="w-100 justify-content-center">
@@ -11,13 +45,13 @@ const Login = () => {
                             <Form>
                                 <Form.Group className="mb-3">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="email@gmail.com"></Form.Control>
+                                    <Form.Control type="email" placeholder="email@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
                                 </Form.Group>
                                 <Form.Group className="mb-3">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Enter Your Password"></Form.Control>
+                                    <Form.Control type="password" placeholder="Enter Your Password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
                                 </Form.Group>
-                                <Button variant="primary" type="submit" className="w-100 py-2 mt-2">Login</Button>
+                                <Button variant="primary" type="submit" className="w-100 py-2 mt-2" disabled={loading}>{loading ? 'Loading...' : "Login"}</Button>
                             </Form>
                         </Card.Body>
                     </Card>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,16 @@
 import { Table, Container, Badge, Card, Row, Col, Button } from "react-bootstrap";
+import UserModal from "../components/UserModal";
+import { useState } from "react";
 
 const User = () => {
+    const [show, setShow] = useState(false);
+    const handleCreate = () => {
+        setShow(true)
+    }
+    const handleCloseModal = () => {
+        setShow(false)
+    }
+    console.log(show);
     return (
         <Container className="py-4">
             <Card className="shadow-sm border-0">
@@ -11,28 +21,33 @@ const User = () => {
                             <p className="text-muted mb-0">Data User Management</p>
                         </Col>
                         <Col xs="auto">
-                            <Button variant="primary">+ Create New User</Button>
+                            <Button variant="primary" onClick={handleCreate}>+ Create New User</Button>
                         </Col>
                     </Row>
 
                     <Table responsive hover bordered className="align-middle">
                         <thead className="table-light">
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Status</th>
-                            <th>Action</th>
+                            <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
                         </thead>
                         <tbody>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
                         </tbody>
                     </Table>
                 </Card.Body>
             </Card>
+            <UserModal show={show} handleClose={handleCloseModal} />
         </Container>
     )
 }
